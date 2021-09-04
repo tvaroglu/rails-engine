67e883bdef7bf715829b4c2a1f4bb4296e8b2499
Taylor Varoglu <tava8398@colorado.edu>
2021-09-04 16:12:14 -0500

    Nest array of resources into data key of JSON hash

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,9 +1,14 @@
 class MerchantSerializer
 
   def self.all(query_params)
+    query_params[:per_page].nil? ? per_page = 20 : per_page = query_params[:per_page].to_i
+    query_params[:page].nil? ? page = 1 : page = query_params[:page].to_i
     # require "pry"; binding.pry
-    query_params[:per_page].nil? ? per_page = 20 : per_page = query_params[:per_page]
-    Merchant.all.limit(per_page)
+    {
+      data: Merchant.all.limit(per_page)
+    }
   end
+
+  def self.format(table); end
 
 end
--- spec/requests/api/v1/merchants/index_spec.rb
+++ spec/requests/api/v1/merchants/index_spec.rb
@@ -5,18 +5,19 @@ describe 'api/v1/merchants#index' do
     create_list(:merchant, 60)
 
     get '/api/v1/merchants'
-    # get '/api/v1/merchants?per_page=20&page=2'
+    # get '/api/v1/merchants?per_page=30&page=2'
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+    json_response = JSON.parse(response.body, symbolize_names: true)
     # require "pry"; binding.pry
-    expect(merchants.count).to eq(20)
+    expect(json_response[:data].count).to eq(20)
   end
 
   xit 'allows for optional query params to customize payload' do
-    # stuff
+    # This should fetch items 51 through 100,
+      # since we’re returning 50 per “page”,
+      # and we want “page 2” of data:
     # GET /api/v1/items?per_page=50&page=2
     # GET /api/v1/merchants?per_page=50&page=2
-    # This should fetch items 51 through 100, since we’re returning 50 per “page”, and we want “page 2” of data.
   end
 end
